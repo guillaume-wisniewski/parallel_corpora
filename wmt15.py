0d21cb45ee38d87cdba91129f8240931e1f4581f
Guillaume Wisniewski <guillaume.wisniewski@univ-paris-diderot.fr>
2021-04-17 21:34:27 +0200

    BPE tokenization

--- wmt15.py
+++ wmt15.py
@@ -6,7 +6,7 @@ from download_corpus import create_directory, lazzy_download, extract_corpus, to
 
 raw_data = create_directory("raw_data")
 download_dir = create_directory("raw_data/downloads")
-working_dir = create_directory("tmp")
+working_dir = create_directory("working_dir")
 
 newsco = lazzy_download("https://www.statmt.org/wmt15/training-parallel-nc-v10.tgz",
                         download_dir / "training-parallel-nc-v10.tgz")
@@ -26,6 +26,18 @@ en_corpora = [
     extract_corpus(europarl, "europarl-v7.fr-en.en", raw_data),
     extract_corpus(common_crawl, "commoncrawl.fr-en.en", raw_data),
 ]
-              
-tokenize_with_spacy(fr_corpora, working_dir / "wmt15.tokenized.fra", "fra")
-tokenize_with_spacy(en_corpora, working_dir / "wmt15.tokenized.eng", "eng")
+
+fra_train_set = tokenize_with_spacy(fr_corpora, working_dir / "wmt15.tokenized.fra", "fra")
+eng_train_set = tokenize_with_spacy(en_corpora, working_dir / "wmt15.tokenized.eng", "eng")
+
+bpe_tokenize(train_fn=fra_train_set,
+             files_to_tokenize=[fra_train_set],
+             tokenized_dir="wmt15/data",
+             vocab_size=32_000,
+             model_prefix="wmt15/model/fra_tokenizer")
+
+bpe_tokenize(train_fn=eng_train_set,
+             files_to_tokenize=[eng_train_set],
+             tokenized_dir="wmt15/data",
+             vocab_size=32_000,
+             model_prefix="wmt15/model/eng_tokenizer")
